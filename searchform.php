934fa45f38c1ad1e641dc9f9ae4ce8b1332227ee
Noriyuki_Kobori <>
2020-08-19 14:37:10 +0900

    検索フォームにscreen-reader-textを追加

--- searchform.php
+++ searchform.php
@@ -1,4 +1,5 @@
 <form class = "p-search-form" id = "searchform" method = "get" action = "<?php echo esc_url(home_url('/')); ?>">
+    <label class = "screen-reader-text" for="s">検索:</label>
     <input class = "c-button__keyword" placeholder = "" name = "s" id = "s">
     <input class = "c-button__submit" id = "searchsubmit" type = "submit" value = "検索">
 </form>
