6528b95484747f05cdea625ed16311a736492923
Noriyuki_Kobori <>
2020-08-19 16:06:28 +0900

    404エラーページを追加

---
+++ 404.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+<main class="l-main">
+    <div id="content" class="narrowcolumn">
+        <p class="l-main__not-found">Error 404 - ページが見つかりません</p>
+    </div>
+</main>
+<?php get_footer(); ?>
+<?php get_sidebar(); ?>
